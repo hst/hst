999512e45c8ad15e316494ffab187dab08529707
Douglas Creager <dcreager@dcreager.net>
2019-08-08 09:52:38 -0400

    Add Process impl for ExternalChoice

--- hst-core/src/external_choice.rs
+++ hst-core/src/external_choice.rs
@@ -17,11 +17,11 @@
 
 use std::fmt::Debug;
 use std::fmt::Display;
-use std::marker::PhantomData;
 
 use smallvec::smallvec;
 use smallvec::SmallVec;
 
+use crate::possibilities::Possibilities;
 use crate::primitives::tau;
 use crate::primitives::Tau;
 use crate::process::Cursor;
@@ -79,21 +79,8 @@ impl<P: Debug + Display> Debug for ExternalChoice<P> {
 #[doc(hidden)]
 #[derive(Clone, Eq, Hash, PartialEq)]
 pub struct ExternalChoiceCursor<E, C> {
-    phantom: PhantomData<E>,
     state: ExternalChoiceState,
-    /// The cursors for the choice's subprocesses.  For each one, we keep a vector of cursors, with
-    /// each element describing the state after an increasingly large number of τ's have been
-    /// performed.  (If a subprocess can't perform any τ's, then there will only ever be a single
-    /// subcursor in the corresponding entry of the outer vector.)
-    ///
-    /// After the choice has been resolved by a visible event, we use the Option part to keep track
-    /// of which of the possible choices are able to perform the sequence of events that have
-    /// occurred since the choice was resolved.  Subprocesses that aren't able to perform the full
-    /// sequence are "eliminated", and retroactively couldn't have been a possible resolution to
-    /// the original choice.
-    subcursors: Vec<Vec<Option<C>>>,
-    /// The number of τ events that have been performed before the choice has been resolved.
-    tau_count: usize,
+    possibilities: Possibilities<E, C>,
 }
 
 #[doc(hidden)]
@@ -103,10 +90,6 @@ pub enum ExternalChoiceState {
     Resolved,
 }
 
-struct Subcursors<'a, C>(&'a Vec<Vec<Option<C>>>);
-struct SubcursorTaus<'a, C>(&'a Vec<Option<C>>);
-struct SubcursorTau<'a, C>(&'a Option<C>);
-
 impl<E, C> Debug for ExternalChoiceCursor<E, C>
 where
     C: Debug,
@@ -114,43 +97,8 @@ where
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         f.debug_struct("ExternalChoiceCursor")
             .field("state", &self.state)
-            .field("tau_count", &self.tau_count)
-            .field("subcursors", &Subcursors(&self.subcursors))
+            .field("subcursors", &self.possibilities)
             .finish()
-    }
-}
-
-impl<'a, C> Debug for Subcursors<'a, C>
-where
-    C: Debug,
-{
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        f.debug_list()
-            .entries(self.0.iter().map(SubcursorTaus))
-            .finish()
-    }
-}
-
-impl<'a, C> Debug for SubcursorTaus<'a, C>
-where
-    C: Debug,
-{
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        f.debug_map()
-            .entries(self.0.iter().map(SubcursorTau).enumerate())
-            .finish()
-    }
-}
-
-impl<'a, C> Debug for SubcursorTau<'a, C>
-where
-    C: Debug,
-{
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self.0 {
-            None => f.write_str("None"),
-            Some(subcursor) => subcursor.fmt(f),
-        }
     }
 }
 
@@ -164,139 +112,8 @@ where
 
     fn root(&self) -> Self::Cursor {
         ExternalChoiceCursor {
-            phantom: PhantomData,
             state: ExternalChoiceState::Unresolved,
-            subcursors: self
-                .0
-                .iter()
-                .map(P::root)
-                .map(Some)
-                .map(|c| vec![c])
-                .collect(),
-            tau_count: 0,
-        }
-    }
-}
-
-impl<E, C> ExternalChoiceCursor<E, C> {
-    /// Deactivates subprocess states based on the number of τ's that they could have performed
-    /// so far, compared to the number of τ's that the choice actually have been performed so far.
-    ///
-    /// More specifically: Each subprocess will have some number of τ's that it could have
-    /// performed, up to a maximum of self.tau_count.  (The subprocess might be willing to perform
-    /// more, but we're about to resolve the choice before allowing it to.)  It's only possible for
-    /// the subprocess to perform `n` τ's if we can find some way for all of the _other_ τ's (i.e.,
-    /// `self.tau_count - n` of them) to be performed by the _other_ τ-eligible subprocesses.  If
-    /// we can't, then we deactivate that subprocess state.
-    fn deactivate_tau_subprocesses(&mut self) {
-        // The total number of τ events performed across all subprocesses at this point.  This will
-        // always be ≥ self.tau_count.  It will be larger if there were multiple subprocesses that
-        // could perform τ's.
-        let total_tau_depth: usize = self.subcursors.iter().map(|taus| taus.len() - 1).sum();
-
-        for taus in &mut self.subcursors {
-            // If this subprocess couldn't perform _any_ τ's, then it remains active.  (We might be
-            // about to deactivate it based on the visible event that resolves that choice, but
-            // it's not being deactivated based on τ's.)
-            if taus.len() == 1 {
-                continue;
-            }
-
-            // The number of τ's this subprocess could have performed.
-            let this_tau_count = taus.len() - 1;
-
-            // The total number of τ's that other subprocesses could have performed.
-            let remaining_tau_depth = total_tau_depth - this_tau_count;
-
-            if remaining_tau_depth < self.tau_count {
-                // The _minimum_ number of τ's this subprocess _must_ perform, to ensure that there
-                // are enough other subprocess τ's to cover the total.
-                let minimum_tau_count = self.tau_count - remaining_tau_depth;
-                for subcursor in taus.iter_mut().take(minimum_tau_count) {
-                    subcursor.take();
-                }
-            }
-        }
-    }
-
-    fn perform_tau_when_unresolved(&mut self, tau: &E)
-    where
-        C: Clone + Cursor<E>,
-    {
-        self.tau_count += 1;
-
-        // For any subprocess that has been able to perform however many τ's occurred _before_
-        // this one, try to have it perform another!  If it can, stash away a copy of the
-        // subcursor describing its state after this τ.
-        for taus in &mut self.subcursors {
-            // This subprocess already stopped being able to perform τ's.  No point checking it
-            // any further!
-            if taus.len() != self.tau_count {
-                continue;
-            }
-
-            // Check whether this subprocess can perform a τ in the current state.
-            let before = taus
-                .last()
-                .expect("Vector should never be empty")
-                .as_ref()
-                .expect("All subprocesses should be active in unresolved external choice");
-            if !before.can_perform(tau) {
-                continue;
-            }
-
-            // If so, grab the subprocess's new state after this τ.
-            let mut after = before.clone();
-            after.perform(tau);
-            taus.push(Some(after));
-        }
-
-        // Deactivate any τ subprocess states that are no longer eligible (because that state has
-        // performed too few τ's, and there aren't enough other subprocesses to make up the
-        // difference).
-        self.deactivate_tau_subprocesses();
-    }
-
-    /// Has each subprocess state that's still activated perform an event, if it can.  If it can't,
-    /// deactivates that state.  Returns whether there were any subprocesses that could perform the
-    /// event.
-    fn perform_and_deactivate(&mut self, event: &E) -> bool
-    where
-        C: Cursor<E>,
-    {
-        let mut any_performed = false;
-        for subcursor in self.subcursors.iter_mut().flatten() {
-            match subcursor {
-                Some(subcursor) if subcursor.can_perform(event) => {
-                    subcursor.perform(event);
-                    any_performed = true;
-                }
-                Some(_) => {
-                    subcursor.take();
-                }
-                _ => {}
-            }
-        }
-        any_performed
-    }
-
-    fn perform_visible_when_unresolved(&mut self, event: &E)
-    where
-        C: Cursor<E>,
-    {
-        // Perform the visible event to actually resolve the choice.  This might deactivate
-        // additional subprocess states, if any of them can't perform this event.
-        self.perform_and_deactivate(event);
-        self.state = ExternalChoiceState::Resolved;
-    }
-
-    fn perform_when_resolved(&mut self, event: &E)
-    where
-        C: Cursor<E>,
-        E: Display,
-    {
-        if !self.perform_and_deactivate(event) {
-            panic!("Resolved external choice cannot perform {}", event);
+            possibilities: Possibilities::new(self.0.iter().map(P::root)),
         }
     }
 }
@@ -307,40 +124,34 @@ where
     C: Clone + Cursor<E>,
 {
     fn events<'a>(&'a self) -> Box<dyn Iterator<Item = E> + 'a> {
-        // Regardless of whether the choice has been resolved yet, we're able to perform any
-        // event allowed by any of the still-eligible possible subprocesses.
-        Box::new(
-            self.subcursors
-                .iter()
-                // One flatten merges the Vecs, the other takes care of the Options.
-                .flatten()
-                .flatten()
-                .flat_map(C::events),
-        )
+        // An external choice can always perform any event that any of its still-activated
+        // subprocesses can perform, regardless of whether the choice has been resolved yet.
+        Box::new(self.possibilities.events())
     }
 
     fn can_perform(&self, event: &E) -> bool {
-        self.subcursors
-            .iter()
-            // One flatten merges the Vecs, the other takes care of the Options.
-            .flatten()
-            .flatten()
-            .any(|subcursor| subcursor.can_perform(event))
+        self.possibilities.can_perform(event)
     }
 
     fn perform(&mut self, event: &E) {
-        match self.state {
-            ExternalChoiceState::Unresolved => {
-                if *event == tau() {
-                    self.perform_tau_when_unresolved(event);
-                } else {
-                    self.perform_visible_when_unresolved(event);
-                }
-            }
-            ExternalChoiceState::Resolved => {
-                self.perform_when_resolved(event);
-            }
+        if self.state == ExternalChoiceState::Resolved {
+            // If the choice has been resolved, pass the event on to any subprocesses that are
+            // still activated.
+            self.possibilities.perform_all(event);
+            return;
         }
+
+        // If the choice has _not_ been resolved, and it's a τ, allow each subprocess to
+        // _independently_ perform the event.
+        if *event == tau() {
+            self.possibilities.perform_piecewise(event);
+            return;
+        }
+
+        // Otherwise, the event resolves the choice!  If there is more than one subprocess that can
+        // perform the event, allow them all to perform it simultaneously.
+        self.possibilities.perform_all(event);
+        self.state = ExternalChoiceState::Resolved;
     }
 }
 
